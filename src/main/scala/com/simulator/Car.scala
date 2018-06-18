8ec82729b16e9d82ef6394467e33085aab05e831
Rafał Mucha <6rafel6@gmail.com>
2018-06-18 18:20:31 +0200

    car- test for current functionality

--- src/main/scala/com/simulator/Car.scala
+++ src/main/scala/com/simulator/Car.scala
@@ -4,24 +4,43 @@ import akka.actor.{Props, Actor, ActorRef}
 import Road._
 
 object Car {
-  def props(car: Car): Props = Props(car)
+  def props(currentPosition:(ActorRef, Double),
+            destinationPosition:(ActorRef, Double),
+            driveAlgorithm: Any): Props =
+    Props(new Car(currentPosition, destinationPosition, driveAlgorithm))
 
-  final case class GetInformationRequest()  //Get information about position, velocity, breaking signal
+  case object CarGetInformationRequest  //Get information about position, velocity, breaking signal
                                               // and number of timeslot
-  final case class GetInformationResult(roadId: ActorRef, position_x: Double, velocity: Double)
+  final case class CarGetInformationResult(roadId: ActorRef,
+                                           position_x: Double,
+                                           velocity: Double,
+                                           breaking: Boolean)
 }
 
-class Car(val supervisorId: ActorRef,
-          currentPosition:(ActorRef, Double),
+class Car(currentPosition:(ActorRef, Double),
           destinationPosition:(ActorRef, Double),
-          var velocity: Double, var breaking: Boolean, val driveAlgorithm: Any) extends Actor {
+          val driveAlgorithm: Any) extends Actor {
 
   import Car._
   var (roadId, positionX) = currentPosition
   val (destinationRoadId, destinationPositionX) = destinationPosition
+  //what to do in time slot
+  var turnToRoad: ActorRef = null
+  var acceleration: Double = 0
+  var velocity: Double = 0
+  var breaking: Boolean = false
+  var synchronizer: Int = -1
+  var current_road_length = (roadId ! GetLength)
 
   def receive = {
-    case GetInformationRequest() =>
-      sender() ! GetInformationResult(roadId, positionX, velocity)
+    case CarGetInformationRequest =>
+      sender() ! CarGetInformationResult(roadId, positionX, velocity, breaking)
+    case ComputeTimeSlot(s) => {
+      new_position = positionX + velocity + acceleration / 2
+      if (new_position - current_road_length > 0) {
+
+      }
+    }
   }
+
 }
