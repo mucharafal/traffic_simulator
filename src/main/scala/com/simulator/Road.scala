550dd332b5ffedfdc789dbd5547df81b34246401
Rafał Mucha <6rafel6@gmail.com>
2018-06-19 23:30:35 +0200

    improvements in synchronizer adjusting other code to changes

--- src/main/scala/com/simulator/Road.scala
+++ src/main/scala/com/simulator/Road.scala
@@ -8,7 +8,7 @@ object Road {
     Props(new Road(startJunction, endJunction, length))
 
   final case class GetNthCar(n: Int)
-  final case class AddCar(car: ActorRef)
+  final case class AddCar(car: ActorRef, time: Double, to: Double)
   final case class RemoveCar(car: ActorRef)
   final case class NthCar(car: Option[ActorRef])
   case object GetEndJunction
@@ -29,12 +29,14 @@ class Road(val startJunction: ActorRef,
   var synchronization: Int = -1
 
   var movementsInTurn = List.empty[(ActorRef, Double, Double)]
+  var addedInTurn = List.empty[(ActorRef, Double, Double)]
 
   def receive = {
     case GetNthCar(n) =>
       sender() ! NthCar(cars.lift(n - 1))
-    case AddCar(ref) =>
+    case AddCar(ref, time, position) =>
       cars :+= ref
+      addedInTurn :+= (sender(), time, position)
     case RemoveCar(ref) =>
       cars = cars.filter(_ != ref)
     case GetEndJunction =>
@@ -45,6 +47,26 @@ class Road(val startJunction: ActorRef,
       movementsInTurn :+= (sender(), from, to)
     case ComputeTimeSlot(s) =>
       synchronization = s
+
+      addedInTurn.sortBy(_._2)
+      var maxPosition: Double = 0.0
+      for(i <- 1 to (movementsInTurn.size-1)){
+        val carA = movementsInTurn(i-1)
+        val carB = movementsInTurn(i)
+
+        if(carA._3 > carB._3) {
+          carA._1 ! Crash
+          carB._1 ! Crash
+        }
+
+        maxPosition = math.max(math.max(carA._3, carB._3), maxPosition)
+      }
+
+      if(!addedInTurn.isEmpty){
+        val max: (ActorRef, Double, Double) = addedInTurn.filter(_ == maxPosition)(0)
+        movementsInTurn :+= (max._1, 0.0, max._3)
+      }
+
       movementsInTurn.sortBy(_._2)
       for(i <- 1 to (movementsInTurn.size-1)){
         val carA = movementsInTurn(i-1)
@@ -55,5 +77,6 @@ class Road(val startJunction: ActorRef,
           carB._1 ! Crash
         }
       }
+
   }
 }
